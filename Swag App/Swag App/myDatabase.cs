f6fdb93f3f417a4de6f682d5d8bddec7c2695104
Apollislauren <102726969+Apollislauren@users.noreply.github.com>
2022-06-03 15:08:53 +0200

    final commit

---
+++ Swag App/Swag App/myDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace Swag_App
+{
+    internal class myDatabase
+    {
+        private SQLiteConnection _database;
+
+        public static myDatabase Instance = new myDatabase();
+
+        public myDatabase()
+        {
+            ///
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            path = path + "swag.db";
+
+            _database = new SQLiteConnection(path, SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache);
+
+            _database.CreateTable<swagProperties>();
+        }
+
+        public List<swagProperties> GetSwag()
+        {
+            return _database.Table<swagProperties>().OrderByDescending(x => x.JokeDate).ToList();
+        }
+
+        public void SaveDadJoke(swagProperties)
+        {
+            _database.Insert(joke);
+        }
+    }
+
+}
---
+++ Swag App/Swag App/swagProperties.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swag_App
+{
+    internal class swagProperties
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string Date { get; set; }
+        public string Address { get; set; }
+    }
+}
